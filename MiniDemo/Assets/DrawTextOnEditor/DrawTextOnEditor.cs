6d2f1fe0d0fcef75811f450190a181d56ce36473
JackieRen <rjq445746468@hotmail.com>
2017-05-02 11:48:36 +0800

    add draw text on editor scene

---
+++ MiniDemo/Assets/DrawTextOnEditor/DrawTextOnEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawTextOnEditor : MonoBehaviour {
+
+    private void OnDrawGizmos() {
+        DrawText(transform.position, transform.name);
+    }
+
+    public void DrawText(Vector3 pos, string text) {
+        Vector3 scrn_pos = Camera.current.WorldToScreenPoint(pos);
+        if (scrn_pos.z < 0f)
+            return;
+
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.black;
+        style.fontSize = (System.Array.IndexOf(UnityEditor.Selection.objects, this.gameObject) != -1) ? 16 : 8;
+        scrn_pos += new Vector3(2, -2, 2);
+        UnityEditor.Handles.Label(Camera.current.ScreenToWorldPoint(scrn_pos), text, style);
+        style.normal.textColor = Color.white;
+        UnityEditor.Handles.Label(pos, text, style);
+    }
+
+}
