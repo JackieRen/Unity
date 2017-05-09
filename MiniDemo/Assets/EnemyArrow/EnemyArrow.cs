06047b2902975d52289e4bdf52fe42fb49e1ee95
JackieRen <rjq445746468@hotmail.com>
2017-05-09 18:00:25 +0800

    add arrow tip

---
+++ MiniDemo/Assets/EnemyArrow/EnemyArrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArrow : MonoBehaviour {
+    public RectTransform arrowCanvas;
+    public UnityEngine.UI.Image arrow;
+
+    public Renderer enemy;
+    
+    void Update() {
+        UpdateArrow(arrowCanvas, enemy.transform.position, arrow);
+        arrow.enabled = !enemy.isVisible;
+    }
+    
+    private void UpdateArrow(RectTransform r, Vector3 enemyWorldPos, UnityEngine.UI.Image arrow) {
+        Vector2 enemyUIPos = UIPosition.WorldToUI(r, enemyWorldPos); 
+
+        float width = r.rect.width / 2;
+        float height = r.rect.height / 2;
+        Vector2 uiPos = new Vector2(Mathf.Clamp(enemyUIPos.x, -width, width), Mathf.Clamp(enemyUIPos.y, -height, height));
+
+        arrow.rectTransform.localPosition = uiPos; 
+        float angle = Mathf.Atan2(uiPos.y, uiPos.x) * Mathf.Rad2Deg;
+        arrow.rectTransform.localEulerAngles = new Vector3(0, 0, angle - 90); 
+    }
+
+}
---
+++ MiniDemo/Assets/EnemyArrow/UIPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPosition {
+
+    static public Vector2 WorldToUI(RectTransform r, Vector3 pos) {
+        Vector2 screenPos = Camera.main.WorldToViewportPoint(pos); 
+        Vector2 viewPos = (screenPos - r.pivot) * 2; 
+        float width = r.rect.width / 2; 
+        float height = r.rect.height / 2;
+        return new Vector2(viewPos.x * width, viewPos.y * height); 
+    }
+
+    static public Vector3 UIToWorld(RectTransform r, Vector3 uiPos) {
+        float width = r.rect.width / 2; 
+        float height = r.rect.height / 2; 
+        Vector3 screenPos = new Vector3(((uiPos.x / width) + 1f) / 2, ((uiPos.y / height) + 1f) / 2, uiPos.z); 
+        return Camera.main.ViewportToWorldPoint(screenPos);
+    }
+
+}
